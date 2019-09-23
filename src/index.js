07462f82e285c21a5e88a0ae252940e4093bdb94
Antti Knutas <antti.knutas@lut.fi>
2019-09-23 10:53:54 +0300

    Edited index
    Added a new comment

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import "./styles.css";
 
-// document.getElementById("app").innerHTML = `
-// <h1>Hello Vanilla!</h1>
-// <div>
-//   We use Parcel to bundle this sandbox, you can find more info about Parcel
-//   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-// </div>
-// `;
+// Added a comment to demonstrate a change
 
 if (document.readyState !== "loading") {
   // Document ready, executing
